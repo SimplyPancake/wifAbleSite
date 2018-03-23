dc4a8b498cd492b9a706302f663b3411e29d45b3
SimplyPancake <danielduikvis@gmail.com>
2018-03-23 08:48:11 +0100

    Fixed some stuffs

--- js/demo.js
+++ js/demo.js
@@ -5,11 +5,11 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#D2E6F3',
-    lineColor: '#FFFBF3',
+    dotColor: '#D2E6F3CC',
+    lineColor: '#FFFBF3BF',
     directionX: 'right',
     directionY: 'up',
-    proximity: 170
+    proximity: 190
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
